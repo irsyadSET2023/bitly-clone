223b084dae1ec9c73edab4e7fc24f26205ec6654
irsyadSET2023 <mirsyad818@gmail.com>
2023-08-11 11:48:41 +0800

    Second  Push

--- src/database/connection/index.js
+++ src/database/connection/index.js
@@ -12,6 +12,15 @@ const postgressConnection = new Sequelize(
     port: data.port,
     dialect: "postgres",
     logging: config.nodeEnv === "development",
+    dialectOptions: {
+      ssl:
+        config.nodeEnv === "production"
+          ? {
+              require: true,
+              rejectUnauthorized: false,
+            }
+          : false,
+    },
   }
 );
 
